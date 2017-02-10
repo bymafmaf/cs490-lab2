3f535b3df0839349a02c842ac833e37e1cb3686d
monus <onus.muvaffak@gmail.com>
2017-02-09 19:32:00 -0500

    - everything is working

--- Lab-TumblrAPI/PhotosViewController.swift
+++ Lab-TumblrAPI/PhotosViewController.swift
@@ -49,7 +49,38 @@ class PhotosViewController: UIViewController, UITableViewDelegate, UITableViewDa
         
         return cell
     }
-
+    func refreshControlAction(_ refreshControl: UIRefreshControl) {
+        
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        
+        let task : URLSessionDataTask = session.dataTask(
+            with: request as URLRequest,
+            completionHandler: { (data, response, error) in
+                if let data = data {
+                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                        with: data, options:[]) as? NSDictionary {
+                        //print("responseDictionary: \(responseDictionary)")
+                        
+                        // Recall there are two fields in the response dictionary, 'meta' and 'response'.
+                        // This is how we get the 'response' field
+                        let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+                        
+                        // This is where you will store the returned array of posts in your posts property
+                        self.posts = responseFieldDictionary["posts"] as! [NSDictionary]
+                        self.mainTableView.reloadData()
+                        refreshControl.endRefreshing()
+                    }
+                }
+        });
+        task.resume()
+    }
+    
     var isLoading: Bool = false
     public func scrollViewDidScroll(_ scrollView: UIScrollView) {
         // Handle scroll behavior here
@@ -121,6 +152,10 @@ class PhotosViewController: UIViewController, UITableViewDelegate, UITableViewDa
         mainTableView.delegate = self
         mainTableView.dataSource = self
 
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        self.mainTableView.insertSubview(refreshControl, at: 0)
+        
         // Do any additional setup after loading the view.
         let url = URL(string:urlString)
         let request = URLRequest(url: url!)
